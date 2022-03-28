1dc1a49015d64be678a98bb67bcb2d427feb7813
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-28 03:51:35 -0400

    Created and tested controller function that uses knex query building to GET a single user’s medication list

--- controllers/medicationsController.js
+++ controllers/medicationsController.js
@@ -1,4 +1,4 @@
-
+const knex = require('knex')(require('../knexfile').development);
 
 
 exports.addMed = (req, res) => {
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,19 @@
-
+const knex = require('knex')(require('../knexfile').development);
 
 
 exports.addUser = (req, res) => {
 
 };
 
-
+// GET All medication (List) from medication table for a specific user_id (foreign key) from the req.paramas in request ('/users/:userId/medications')
 exports.medList = (req, res) => {
-
+    knex.from('medication') // table. Can be written as knex('medication') too
+        .select('*') // automatically selects all even if not specified
+        .where({ user_id: req.params.userId })
+        .then((medList) => {
+            res.status(200).json(medList)
+        })
+        .catch(err => {
+            res.send(500).json(`Error getting Med List for user_id: ${req.params.userId}. Error: ${err}`)
+        })
 };
